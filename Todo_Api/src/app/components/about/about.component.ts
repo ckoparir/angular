098bdbde2da2c2ed0d719c41c3cfe528647e25be
ckoparir <ckoparir@gmail.com>
2019-03-20 21:39:00 +0300

    no change

--- Todo_Api/src/app/components/about/about.component.ts
+++ Todo_Api/src/app/components/about/about.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.less']
+  // templateUrl: './about.component.html',
+  // TODO: Example for inline template for the component
+  template: `
+    <p>This is inline template paragraph within component</p>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./about.component.less'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
